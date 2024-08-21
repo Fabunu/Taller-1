6c480dbf3e9aafd299a2d5ad2c7efffa4198150f
Fabunu <f.cheuquelaf01@ufromail.cl>
2024-08-21 10:29:14 -0400

    Algunos cambios

--- JuegoDeCartas.java
+++ JuegoDeCartas.java
@@ -30,5 +30,45 @@ public class JuegoDeCartas {
 
         } while(opcion =! 2);
     }
-    
+
+    public static String[][] crearMatrizCartas(){
+        String matrizCartas[][] = new String [4][12];
+        inicializarCartasJuego(matrizCartas);
+        return matrizCartas;
+
+    }
+
+    public static String[][] inicializarCartasJuego(String matrizCartas[][]) {
+        
+        String palos[] = {"Corazon","Pica","Diamante","Trebol"};
+        String valores[] = {"A","2","3","4","5","6","7","8","9","J","Q","K"};
+        String puntos[] = {"11","2","3","4","5","6","7","8","9","10","10","10"};
+
+        for(int i = 0; i < palos.length; i++) {
+            for(int j = 0; i < valores.length; i++) {
+                matrizCartas[i][j] = valores[j] + "de" + palos[i] + "puntos" + puntos[j] ;
+            }
+        }
+    }
+
+    public static int obtenerCartas(String matrizCartas[][]) {
+        Random random = new Random();
+        int palo = random.nextInt(4);
+        int valor = random.nextInt(12);
+
+        String carta = matrizCartas[palo][valor];
+        System.out.println("Carta obtenida" + carta);
+
+        String partesCarta[] = carta.split("");
+        int puntos = Integer.parseInt(partesCarta[partesCarta.length-2]);
+        return puntos;
+    }
+
+    public static void jugar(){
+        String matrizCartas[][] = crearMatrizCartas();
+        Scanner scanner = new Scanner(System.in);
+        int puntajeJugador1 = 0;
+        int puntajeJugador2 = 0;
+        
+    }
 }
